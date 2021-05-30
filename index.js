cde021d7d2752154477cca9d3ab81034eddf9ee2
titiunescau <81128483+titiunescau@users.noreply.github.com>
2021-05-30 15:47:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -899,7 +899,80 @@ if (text.includes("placa"))
                  if (args.length < 1) return reply(`Use dessa forma:\nComando: ${prefix}attp Toin gado`)
                  attp2 = await getBuffer(`https://api.xteam.xyz/attp?file&text=${encodeURIComponent(body.slice(5))}`)
                  client.sendMessage(from, attp2, sticker, {quoted: mek})
-                        break
+                        break 
+					const fs = require('fs');
+
+//_NOME DO CRIADOR DOS STICKERS
+authorname = 'ZENITSU-BOT'
+packname = 'tiu Nescau'
+  
+function addMetadata(packname, author) {	
+if (!packname) packname = 'ZENITSU-BOT'; if (!author) author = 'Bot';	
+author = author.replace(/[^a-zA-Z0-9]/g, '');	
+let name = `${author}_${packname}`
+if (fs.existsSync(`./src/stickers/${name}.exif`)) return `./src/stickers/${name}.exif`
+const json = {	
+"sticker-pack-name": packname,
+"sticker-pack-publisher": author,
+}
+const littleEndian = Buffer.from([0x49, 0x49, 0x2A, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57, 0x07, 0x00])	
+const bytes = [0x00, 0x00, 0x16, 0x00, 0x00, 0x00]	
+let len = JSON.stringify(json).length	
+let last	
+if (len > 256) {	
+len = len - 256	
+bytes.unshift(0x01)	
+} else {	
+bytes.unshift(0x00)	
+}	
+if (len < 16) {	
+last = len.toString(16)	
+last = "0" + len	
+} else {	
+last = len.toString(16)	
+}	
+const buf2 = Buffer.from(last, "hex")	
+const buf3 = Buffer.from(bytes)	
+const buf4 = Buffer.from(JSON.stringify(json))	
+const buffer = Buffer.concat([littleEndian, buf2, buf3, buf4])	
+fs.writeFile(`./src/stickers/${name}.exif`, buffer, (err) => {	
+return `./src/stickers/${name}.exif`	
+})	
+}
+
+module.exports = { addMetadata }
+					case 'openg':
+  case 'abrirg':
+client.updatePresence(from, Presence.composing)
+if (!isGroup) return reply(ptbr.group())
+if (!isGroupAdmins) return reply(ptbr.admin())
+if (!isBotGroupAdmins) return reply(ptbr.Badmin())
+open = {
+  text: `Grupo aberto por: @${sender.split("@")[0]}`,
+  contextInfo: {
+mentionedJid: [sender]
+  }
+}
+client.groupSettingChange (from, GroupSettingChange.messageSend, false)
+client.sendMessage(from, open, text, {
+  quoted: mek
+})
+break
+case 'fecharg':
+client.updatePresence(from, Presence.composing)
+if (!isGroup) return reply(ptbr.group())
+if (!isGroupAdmins) return reply(ptbr.admin())
+if (!isBotGroupAdmins) return reply(ptbr.Badmin())
+var nomor = mek.participant
+const close = {
+text: `Grupo fechado por: @${nomor.split("@s.whatsapp.net")[0]}`,
+contextInfo: {
+mentionedJid: [nomor]
+}
+}
+client.groupSettingChange (from, GroupSettingChange.messageSend, true);
+reply(close)
+break
                 case 'eu':
                 putagg = await getBuffer(`https://i.ibb.co/TthtCSG/pakipariu-doido.jpg`)
                 client.sendMessage(from, putagg, buffer, {quoted: mek, caption: 'Vc'})
