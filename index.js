4577faba9ee71b05668f493c76582b04454225c3
titiunescau <81128483+titiunescau@users.noreply.github.com>
2021-06-14 15:15:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -247,7 +247,7 @@ Bem Vindo Ao Grupo! Olhe As Regras Do grupo Para Não Ser Banido
             }
             if (anu.action == 'remove') {
                 ini_user = client.contacts[num]
-                ini_img = await getBuffer(`https://api-exteam.herokuapp.com/api/goodbye?titulo=ADEUS&nome=${pushname2}&perfil=${psCAPA.link}=https://i.imgur.com/mEGIfin.png&fundo=https://i.imgur.com/fbs4CDb.jpg&grupo=SAIU%20DE%&gpnome=${encodeURIComponent(mdata.subject)}`)
+                ini_img = await getBuffer(`https://api-exteam.herokuapp.com/api/goodbye?titulo=ADEUS&nome=sender&perfil=${psCAPA.link}=https://i.imgur.com/mEGIfin.png&fundo=https://i.imgur.com/fbs4CDb.jpg&grupo=SAIU%20DE%&gpnome=${encodeURIComponent(mdata.subject)}`)
                 client.sendMessage(anu.jid, ini_img, MessageType.image)
             }
                 } catch (e) {
