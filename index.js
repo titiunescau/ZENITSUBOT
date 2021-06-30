021d832bbb9d157e28dcb0f21af96983a0684b42
titiunescau <81128483+titiunescau@users.noreply.github.com>
2021-06-30 20:43:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -932,9 +932,22 @@ if (text.includes("placa"))
                 const nescau = await getBuffer(getFoto)
                 client.sendMessage(from, nescau, image, {quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": "⚡Zenitsu⚡", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('./sticker/kkk.webp')} } }, caption: help(prefix, sender, pushname, time)})
                   break 
-
-
-                break
+case 'msg':
+			if (isGroupMsg && isGroupAdmins || isGroupMsg && isOwner) {
+				if (args.length !== 1) return kill.reply(from, 'Você esqueceu de colocar se quer ativado [on], ou desativado [off].', id)
+				if (args[0] == 'on') {
+					faki.push(groupId)
+					fs.writeFileSync('./lib/config/fake.json', JSON.stringify(faki))
+					await kill.reply(from, 'Anti-Fakes habilitado.', id)
+				} else if (args[0] == 'off') {
+					faki.splice(groupId, 1)
+					fs.writeFileSync('./lib/config/fake.json', JSON.stringify(faki))
+					await kill.reply(from, 'Anti-fakes desabilitado.', id)
+				}
+            } else {
+                await kill.reply(from, mess.error.Ga, id)
+            }
+            break
 					case 'amor':
                 if (mek.message.extendedTextMessage === undefined || mek.message.extendedTextMessage === null) return reply('Marque a pessoa')
 				mentidn = mek.message.extendedTextMessage.contextInfo.mentionedJid[0]
